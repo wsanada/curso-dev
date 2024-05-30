6503895b0f7e1eeebe139119224920766ebde054
Willian K. Sanada <41298785+wsanada@users.noreply.github.com>
2024-05-30 21:30:59 +0000

    database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   res.status(200).json({ chave: "Tudo certo!" });
 }
