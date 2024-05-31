701be59b7c7f79f7752d5d9f7977f158529b0a25
Willian K. Sanada <41298785+wsanada@users.noreply.github.com>
2024-05-31 18:52:54 +0000

    partial implementation

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,36 @@
 import database from "infra/database.js";
 
-export default async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "Tudo certo!" });
+async function status(req, res) {
+  const databaseName = process.env.POSTGRES_DB;
+
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(0)::int from pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
+  });
 }
+
+export default status;
