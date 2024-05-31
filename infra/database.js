944d4113e7f66e197583887bceff838196ba802d
Willian K. Sanada <41298785+wsanada@users.noreply.github.com>
2024-05-31 21:02:49 +0000

    Partial implementation

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.POSTGRES_SSLMODE === "require",
-  });
-
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.POSTGRES_SSLMODE === "require",
+    ssl: process.env.NODE_ENV !== "development",
   });
 
   try {
